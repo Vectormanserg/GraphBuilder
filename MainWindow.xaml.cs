bc781306da85e6d2fec8625464683b2cb83ba4a8
Соснин Сергей Александрович <vectormanserg@mail.ru>
2020-07-03 15:14:50 +0300

    Added App.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -162,37 +162,4 @@ namespace GraphBuilder
             }
         }
     }
-    public partial class MathParser
-    {
-        public static string[] funcToArr(string func)
-        {
-            func = "x^2+3(x+x^3)-5cosx";
-            string[] parsedArr = new string[func.Length];
-            for (int i = 0; i < func.Length; i++)
-            {
-                parsedArr[i] = func[i].ToString();
-            }
-            return parsedArr;
-        }
-        public static double Parser(string func)
-        {
-            string[] funcArr = funcToArr(func);
-            double result;
-            string str;
-            
-            for (int n = 0; n < funcToArr(func).Length; n++)
-            {
-                if (funcArr[n] == "(")
-                {
-                    do
-                    {
-                        str = funcArr[n] + funcArr[n + 1];
-                        n += 2;
-                    }
-                    while (funcArr[n] != ")");
-                }
-            }
-            return 1;
-        }
-    }
 }
