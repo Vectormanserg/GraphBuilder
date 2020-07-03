85b972066dae2e05d12f89190e18f60ff490afbe
Vectormanserg <vectormanserg@gmail.com>
2020-07-03 16:51:48 +0300

    Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,4 +14,33 @@ namespace GraphBuilder
     public partial class App : Application
     {
     }
+    public partial class MathParser
+    {
+        public static double Parser(string func)
+        {
+            double result;
+            if (func.Contains("(") && func.Contains(")"))
+            {
+                result = Skobki(func);
+            }
+            else result = BezSkobok(func);
+            return result;
+        }
+        public static double BezSkobok(string func)
+        {
+            List<string> Sub = new List<string>();
+            double result;
+            string Temp = func;
+            //for (int i = 0;)
+            return result;
+        }
+        public static double Skobki(string func)
+        {
+            List<string> Sub = new List<string>();
+            double result;
+            string Temp = func;
+
+            return result;
+        }
+    }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -137,7 +137,7 @@ namespace GraphBuilder
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MathParser.Parser(txtFunc.Text);
+
         }
 
         private void defTxtFunc(object sender, RoutedEventArgs e)
@@ -160,39 +160,6 @@ namespace GraphBuilder
             {
                 txtXmax.Text = "Введите максимальный Х";
             }
-        }
-    }
-    public partial class MathParser
-    {
-        public static string[] funcToArr(string func)
-        {
-            func = "x^2+3(x+x^3)-5cosx";
-            string[] parsedArr = new string[func.Length];
-            for (int i = 0; i < func.Length; i++)
-            {
-                parsedArr[i] = func[i].ToString();
-            }
-            return parsedArr;
-        }
-        public static double Parser(string func)
-        {
-            string[] funcArr = funcToArr(func);
-            double result;
-            string str;
-            
-            for (int n = 0; n < funcToArr(func).Length; n++)
-            {
-                if (funcArr[n] == "(")
-                {
-                    do
-                    {
-                        str = funcArr[n] + funcArr[n + 1];
-                        n += 2;
-                    }
-                    while (funcArr[n] != ")");
-                }
-            }
-            return 1;
         }
     }
 }
